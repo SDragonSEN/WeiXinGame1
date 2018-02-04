71ecfff088ab2a30f02c208b5632d216ab59b709
SDragonSEN <SDragonSEN@163.com>
2018-02-04 17:08:41 +0800

    new file:   src/base/pool.js 	new file:   src/obj/rec.js 	new file:   src/obj/wall.js 	new file:   src/sprits/body.js 	new file:   src/sprits/snake.js

---
+++ src/sprits/snake.js
@@ -0,0 +1,8 @@
+const screenWidth = window.innerWidth
+const screenHeight = window.innerHeight
+export default class Snake {
+  constructor() {
+    this.x = screenWidth/2;
+    this.y = screenHeight*4/5;
+  }
+}
